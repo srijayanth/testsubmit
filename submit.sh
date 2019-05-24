cacfa0ced984260656a01a435c73e34d0762aae3
srijayanth <36004266+srijayanth@users.noreply.github.com>
2019-05-24 17:11:55 +0530

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,7 +1,8 @@
 oldpwd=`pwd`
+actual_dir=$(basename `pwd`)
 url="http://10.132.20.23:8090/submit"
 cd ..
-tar cvfz ./submission.tar.gz sumOfMaxSequence
+tar cvfz ./submission.tar.gz ${actual_dir}
 username=`whoami`
 curl -X POST ${url}\?username\=${username} --data-binary @submission.tar.gz
 rm ./submission.tar.gz
