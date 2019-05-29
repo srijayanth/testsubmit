368f27e85f8688e5109c4b92a0e31f5cf8bc766f
srijayanth <36004266+srijayanth@users.noreply.github.com>
2019-05-29 14:41:14 +0530

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,9 +1,4 @@
-oldpwd=`pwd`
-actual_dir=$(basename `pwd`)
-url="http://bcfbb1ad.ngrok.io/submit"
-cd ..
-tar cvfz ./submission.tar.gz ${actual_dir}
-username=`whoami`
-curl -X POST ${url}\?username\=${username} --data-binary @submission.tar.gz
-rm ./submission.tar.gz
-cd $oldpwd
+mkdir -p ~/bin
+cp ~/Downloads/submit ~/bin
+chmod +x ~/bin/submit
+echo "export PATH=\"\$HOME/bin:\$PATH\""
